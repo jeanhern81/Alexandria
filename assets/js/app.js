2b3464193f6fbe62d44623c71a847381c19f34cb
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-06-19 23:19:59 -0700

    minor changes attempting to output from getLatLong function

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
 		addressArr[0].address + addressArr[0].city + addressArr[0].state;
 	// function that takes in an address and returns the latitude and longitude from the google geocode api
 
-	function getLatLong(a) {
+	var getLatLong = function (a, callback) {
 		var queryURL =
 			"https://maps.googleapis.com/maps/api/geocode/json?address=" +
 			a +
@@ -40,22 +40,105 @@ $(document).ready(function () {
 			method: "GET",
 		}).then(function (response) {
 			var latlon = response.results[0].geometry.location;
-			return latlon;
+			callback(latlon);
 			// Create and save a reference to new empty table row
 			// Create and save references to 3 td elements containing the Title, Year, and Actors from the AJAX response object
 			// Append the td elements to the new table row
 			// Append the table row to the tbody element
 		});
-	}
+	};
+	var coordinates;
 
-	getLatLong(newAddress);
+	getLatLong(newAddress, function (latlon) {
+		coordinates = latlon;
+	});
+	console.log(coordinates);
+	// var latLon = function () {
+	// 	$.ajax({
+	// 		url:
+	// 			"https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDHRCqL8yZbKNEZl7PFCmbA_XlaIBluHZ8",
 
-	// var map;
-	// function initMap(b) {
-	// 	map = new google.maps.Map($("#map"), {
-	// 		center: b,
-	// 		zoom: 8,
+	// 		data: {
+	// 			address: newAddress,
+	// 		},
+
+	// 		dataType: "json",
+
+	// 		success: function (r) {
+	// 			if ("success") {
+	// 				console.log(JSON.stringify(r.results[0].geometry.location));
+	// 				$("#latLon").text(JSON.stringify(r.results[0].geometry.location));
+	// 			}
+	// 		},
+	// 		error: function (e) {
+	// 			if ("error") {
+	// 				return e;
+	// 			}
+	// 		},
 	// 	});
-	// }
-	// initMap(latlon);
+	// };
+	// console.log(latLon);
 });
+// 	var geocoder;
+// 	var map;
+// 	var address = "San Diego, CA";
+
+// 	function initialize() {
+// 		geocoder = new google.maps.Geocoder();
+// 		var latlng = new google.maps.LatLng(-34.397, 150.644);
+// 		var myOptions = {
+// 			zoom: 8,
+// 			center: latlng,
+// 			mapTypeControl: true,
+// 			mapTypeControlOptions: {
+// 				style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+// 			},
+// 			navigationControl: true,
+// 			mapTypeId: google.maps.MapTypeId.ROADMAP,
+// 		};
+// 		map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+// 		if (geocoder) {
+// 			geocoder.geocode(
+// 				{
+// 					address: address,
+// 				},
+// 				function (results, status) {
+// 					if (status == google.maps.GeocoderStatus.OK) {
+// 						if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
+// 							map.setCenter(results[0].geometry.location);
+
+// 							var infowindow = new google.maps.InfoWindow({
+// 								content: "<b>" + address + "</b>",
+// 								size: new google.maps.Size(150, 50),
+// 							});
+
+// 							var marker = new google.maps.Marker({
+// 								position: results[0].geometry.location,
+// 								map: map,
+// 								title: address,
+// 							});
+// 							google.maps.event.addListener(marker, "click", function () {
+// 								infowindow.open(map, marker);
+// 							});
+// 						} else {
+// 							alert("No results found");
+// 						}
+// 					} else {
+// 						alert(
+// 							"Geocode was not successful for the following reason: " + status
+// 						);
+// 					}
+// 				}
+// 			);
+// 		}
+// 	}
+// 	google.maps.event.addDomListener(window, "load", initialize);
+// 	// var map;
+// 	// function initMap(b) {
+// 	// 	map = new google.maps.Map($("#map"), {
+// 	// 		center: b,
+// 	// 		zoom: 8,
+// 	// 	});
+// 	// }
+// 	// initMap(latlon);
+// });
