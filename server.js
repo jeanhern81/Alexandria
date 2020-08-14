6305c42b5c55f81ea032ef56eb0921545b76ac8b
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-08-13 19:17:15 -0700

    working with path. able to display index.html file only.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
+var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -20,14 +21,20 @@ app.use(express.static("public"));
 
 // Routes - Need to deteremine what routes we'll be adding
 // =============================================================
-require("")(app);
-require("")(app);
-require("")(app);
+// require("")(app);
+// require("")(app);
+// require("")(app);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function () {
-  app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-  });
+// db.sequelize.sync({ force: true }).then(function () {
+//   app.listen(PORT, function () {
+//     console.log("App listening on PORT " + PORT);
+//   });
+// });
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
 });
