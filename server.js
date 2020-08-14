3de08ac31508c42f469585b2fe98abc20d1f0a08
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-08-13 20:48:04 -0700

    working on localhost

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/properties.html"));
 });
 
 // Starts the server to begin listening
