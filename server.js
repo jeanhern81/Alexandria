e40777c25e05cf46e6228cede3c93947c6dadc3b
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-08-13 21:26:35 -0700

    index.html working with styling.

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // Routes
 // ===============================================
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/properties.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Starts the server to begin listening
