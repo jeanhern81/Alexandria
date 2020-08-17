86f55d6e9629cd975996402cfdd7edf7e704774b
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-08-17 13:12:59 -0700

    changes before merge

--- models/index.js
+++ models/index.js
@@ -24,7 +24,7 @@ fs
     })
     .forEach(function (file) {
         console.log(path.join(__dirname, file))
-        var model = sequelize['import'](path.join(__dirname, file));
+        var model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
         db[model.name] = model;
     });
 
