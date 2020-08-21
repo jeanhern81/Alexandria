c977a6726b8be9facc142322a72c7d5bbb56ba48
jeanhern81 <jeanhern81@gmail.com>
2020-08-20 22:39:28 -0700

    heroku deployment

--- config/config.js
+++ config/config.js
@@ -1,20 +1,31 @@
-module.exports = {
-  "development": {
-    "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_KEY,
-    "database": process.env.MYSQL_DBNAME,
-    "host": process.env.MYSQL_HOST,
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
-  }
+var mysql = require("mysql");
+require("dotenv").config();
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL); 
+
+} else {
+  connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Mercury35!",
+  database: "alexandria_db"
+  });
 }
+
+
+
+
+// Make connection.
+connection.connect(function(err) {
+if (err) {
+  console.error("error connecting: " + err.stack);
+  return;
+}
+console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
