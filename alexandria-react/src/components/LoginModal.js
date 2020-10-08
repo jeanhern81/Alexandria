90220187d0fad6a5a01db98be9f3fa717264ae5e
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-10-07 18:07:51 -0700

    reworked the login component to use hooks and added proxy to react dev server to fix cors issues when connecting to express server

--- alexandria-react/src/components/LoginModal.js
+++ alexandria-react/src/components/LoginModal.js
@@ -1,62 +1,85 @@
-import React, { Component } from 'react';
-import{ Modal, Button, Form, Image } from 'react-bootstrap';
+import React, { Component, useState } from 'react';
+import { Modal, Button, Form, Image } from 'react-bootstrap';
+import $ from "jquery";
 
-export class LoginModal extends Component {
-    constructor(props){
-        super(props);
-        
-    };
-    
-render(){
-            return(
-            <Modal
-        {...this.props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-    >
-        <Modal.Header closeButton>
-        <Image src={require('../images/Alexandria-logo-s.png') } style={{float:'' }} id='loginAlexLogo' alt='Alexandria Logo'/>    
-        
-        <Modal.Title id="contained-modal-title-vcenter">
 
-            
-        </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
+export default function Login(props) {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [user, setUser] = useState("");
+    const [userId, setUserId] = useState("");
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log({ email }, { password });
+        $.ajax({ url: "/api/users/login", method: "POST", data: { email: email, password: password } })
+            .then(res => {
+                const response = res;
+                const id = response._id;
+                const user = response.email;
+                console.log(id)
+                setUserId(id);
+
+
+
+            }).then()
+    }
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Image src={require('../images/Alexandria-logo-s.png')} style={{ float: '' }} id='loginAlexLogo' alt='Alexandria Logo' />
+
+                <Modal.Title id="contained-modal-title-vcenter">
+
+
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
                 <p><h3>Login</h3> </p>
-        <p><h5 className="text-center">Welcome Back!</h5></p>
-        <Form>
-    <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
+                <p><h5 className="text-center">Welcome Back!</h5></p>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email"
+                            onChange={e => setEmail(e.target.value)} />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
     </Form.Text>
-    </Form.Group>
+                    </Form.Group>
 
-    <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-    Login
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password"
+                            onChange={e => setPassword(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Login
     </Button>
-            <p clasName='newCustomer'> New Customer? <a href="signUp.home.html">Create account</a></p>
+                    <p clasName='newCustomer'> New Customer? <a href="signUp.home.html">Create account</a></p>
 
-</Form>
+                </Form>
 
-        </Modal.Body>
-        <Modal.Footer>
-        <Button onClick={this.props.onHide}>Close</Button>
-        </Modal.Footer>
-    </Modal>
-        );
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
 }
 
-    };
 
-export default LoginModal;
+
+
