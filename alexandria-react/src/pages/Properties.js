215bd1a89570f53070d3ad59d3409f9ec71e9fa5
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-10-12 11:21:33 -0700

    started properties page.

--- alexandria-react/src/pages/Properties.js
+++ alexandria-react/src/pages/Properties.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 import NavBarProps from "../components/NavBarProps";
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
+import { Container, Row } from "react-bootstrap";
 
-import '../index.css';
+import "../index.css";
 import "./Properties.css";
 
-
 function Properties() {
+  return (
+    <div>
+      <NavBarProps />
 
+      <Container fluid>
+        <Row>
+          <h5 style={{ float: "right" }} id="property-header">
+            PROPERTIES
+          </h5>
+          {/* <!-- Beginning of Property listings. This Section will need to be dynamic with JS. --> */}
+          {/* <!-- Propertery #1 - Just a place holder--> */}
+        </Row>
+        <hr />
+      </Container>
 
-        return (
-            <div>
-                <NavBarProps />
+      <Footer />
+    </div>
+  );
+}
 
-                <h1> Props Page Works!</h1>
-
-                <Footer />
-            </div>
-        );
-    }
-
-
-
-export {Properties}
+export { Properties };
