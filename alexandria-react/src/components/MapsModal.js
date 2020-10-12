5fe29283086bf637eed8443f4114f27a79f5cc27
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-10-12 15:55:17 -0700

    maps modal working.

--- alexandria-react/src/components/MapsModal.js
+++ alexandria-react/src/components/MapsModal.js
@@ -1,30 +1,20 @@
 import React, { Component } from "react";
-import { Modal, Button, Image, Container } from "react-bootstrap";
+import { Modal, Button, Image } from "react-bootstrap";
 import { Map, GoogleApiWrapper } from "google-maps-react";
-import "../index.css";
 
 const mapStyles = {
-  height: 500,
-  width: 500,
-  float: "left",
-  backgroundColor: "#F4A460",
-  position: "relative",
+  height: "400%",
+  width: "100%",
 };
 
-export class MapsModal extends Component {
+export class MapsModal extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
-      <Modal
-        {...this.props}
-        size="lg"
-        dialogClassName="modal-90w"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
+      <Modal {...this.props} size="lg">
         <Modal.Header closeButton>
           <Image
             src={require("../images/Alexandria-logo-s.png")}
@@ -35,14 +25,14 @@ export class MapsModal extends Component {
 
           <Modal.Title id="contained-modal-title-vcenter"></Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <Map
-            google={this.props.google}
-            zoom={8}
-            style={mapStyles}
-            initialCenter={{ lat: 47.444, lng: -122.176 }}
-          />
-        </Modal.Body>
+        {/* <Modal.Body> */}
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176 }}
+        />
+        {/* </Modal.Body> */}
         <Modal.Footer>
           <Button onClick={this.props.onHide}>Close</Button>
         </Modal.Footer>
